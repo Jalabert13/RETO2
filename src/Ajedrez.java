1fea690f7f5829e5da2f964afb71c049717fcbb7
juanc <juancarlos.rabasco@gmail.com>
2023-05-18 00:07:14 +0200

    Lectura ficheros y mas

--- src/Ajedrez.java
+++ src/Ajedrez.java
@@ -15,7 +15,7 @@ public class Ajedrez {
             //La variable i se utiliza para asignar de manera ascendente a cada jugador el puesto de su premio, ya que la consulta los muestra de manera ascendente
             //Insercion C.Val, Torneo A
             int i = 1;
-            for (; rs_a.next() && i<=8; i++){
+            for (; rs_a.next(); i++){
                 name = rs_a.getString("nom_jugador");
                 pstm = cnx.prepareStatement("UPDATE premios SET nom_jugador = ? WHERE puesto_premio = ? AND nom_torneo = 'A' AND tipo = 'C.VALENCIANA'");
                 pstm.setString(1, name);
@@ -30,7 +30,7 @@ public class Ajedrez {
             ResultSet rs_b = stm.executeQuery(query_b);
             //Insercion C.Val, Torneo B
             int j = 1;
-            for (; rs_b.next() && j<=8; j++){
+            for (; rs_b.next(); j++){
                 name = rs_b.getString("nom_jugador");
                 pstm = cnx.prepareStatement("UPDATE premios SET nom_jugador = ? WHERE puesto_premio = ? AND nom_torneo = 'B' AND tipo = 'C.VALENCIANA'");
                 pstm.setString(1, name);
@@ -49,6 +49,31 @@ public class Ajedrez {
 
         }catch (SQLException e){
             throw new RuntimeException(e);
+        }
+    }
+
+
+    //Insercion premios por clasificacion general, introduciendo por parametro el nombre del torneo
+    private static void insertGen(String torneo){
+        try(Connection cnx = Con.getConnexion()){
+            PreparedStatement pstm = cnx.prepareStatement("SELECT nom_jugador FROM gen_clasifica WHERE jug_torneo = ? ORDER BY puesto ASC LIMIT 25");
+            pstm.setString(1, torneo);
+            ResultSet rs = pstm.executeQuery();
+
+            String name;
+            int i = 1;
+            for (; rs.next(); i++){
+                name = rs.getString("nom_jugador");
+                pstm = cnx.prepareStatement("UPDATE premios SET nom_jugador = ? WHERE puesto_premio = ? AND nom_torneo = ? AND tipo = 'GENERAL'");
+                pstm.setString(1, name);
+                pstm.setInt(2, i);
+                pstm.setString(3, torneo);
+                pstm.executeUpdate();
+                System.out.println("Jugador insertado");
+            }
+
+        }catch (SQLException ex){
+            throw new RuntimeException(ex);
         }
     }
 
--- src/Con.java
+++ src/Con.java
@@ -5,9 +5,9 @@ import java.sql.SQLException;
 
 public class Con {
     static Connection getConnexion() throws SQLException {
-        String url = "jdbc:mariadb://localhost:3306/Ajedrez_empty";
+        String url = "jdbc:mariadb://localhost:3306/Ajedrez";
         String user = "root";
-        String password = "root";
+        String password = "";
         return DriverManager.getConnection(url, user, password);
     }
 }
